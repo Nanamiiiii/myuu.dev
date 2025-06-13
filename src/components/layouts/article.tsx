ace62875fdea2372b5fe43383627444d84a4c3f8
Akihiro Saiki <sk@myuu.dev>
2025-06-13 14:04:23 +0900

    fix for new framer-motion ver

--- src/components/layouts/article.tsx
+++ src/components/layouts/article.tsx
@@ -15,7 +15,7 @@ const Layout = ({ children }: { children: any }) => {
       animate="enter"
       exit="exit"
       variants={variants}
-      transition={{ duration: 0.4, type: 'easeInOut' }}
+      transition={{ duration: 0.4, ease: 'easeInOut' }}
       style={{ position: 'relative' }}
     >
       <>
